c9d299b2c79a1485bf35cb641224ebaf2bcf84bf
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-11 16:50:50 +0700

    [ADD] redirect to formulir/login

--- seller/beranda-mitra/index.php
+++ seller/beranda-mitra/index.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['id_toko'])) {
+    header("Location: ../beranda-mitra");
+} elseif (isset($_SESSION['id'])) {
+    header("Location: ../login");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
