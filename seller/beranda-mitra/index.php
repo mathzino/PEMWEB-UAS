5b8c6cf0ec4c4719b4ceb56ccb8bc750c17c8fbf
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-11 16:52:37 +0700

    [FIX] typo beranda-mitra -> formulir-mitra

--- seller/beranda-mitra/index.php
+++ seller/beranda-mitra/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['id_toko'])) {
-    header("Location: ../beranda-mitra");
-} elseif (isset($_SESSION['id'])) {
+    header("Location: ../formulir-mitra");
+} elseif (!isset($_SESSION['id'])) {
     header("Location: ../login");
 }
 ?>
