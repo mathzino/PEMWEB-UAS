58af29dbfb64483038f6280e620ba975202b51be
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-11 16:50:27 +0700

    [FIX] add id_toko to SESSION from seller login

--- seller/login/verif_login.php
+++ seller/login/verif_login.php
@@ -12,7 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (password_verify($password, $row['password'])) {
                 $_SESSION['id'] = $row['id_seller'];
                 $_SESSION['username'] = $row['username'];
-                header("Location:../formulir-mitra");
+
+                $query_toko  = "SELECT * FROM toko WHERE owner = '$row[id_seller]'";
+                $result_toko = mysqli_query(connection(), $query_toko);
+                if (mysqli_num_rows($result_toko) == 1) {
+                    while ($row_toko = mysqli_fetch_assoc($result_toko)) {
+                        $_SESSION['id_toko'] = $row_toko['toko_id'];
+                    }
+                    header("Location:../beranda-mitra");
+                } else {
+                    header("Location:../formulir-mitra");
+                }
             } else {
                 $status = "invalid";
                 header("Location: index.php?status=" . $status);
