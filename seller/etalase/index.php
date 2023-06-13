bfbbe58425ad1105a361d264deec5d5a64dc7516
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-13 09:28:17 +0700

    [ADD] etalase, stats

--- seller/etalase/index.php
+++ seller/etalase/index.php
@@ -1,72 +1,93 @@
+<?php
+session_start();
+include '../../conn.php';
+if (!isset($_SESSION['id_toko'])) {
+	header("Location: ../formulir-mitra");
+} elseif (!isset($_SESSION['id'])) {
+	header("Location: ../login");
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title></title>
 </head>
 <link rel="stylesheet" type="text/css" href="style-etalase.css">
+
 <body>
 
 	<div class="etalase">
 		<div class="background-atas">
-		<div class="toko-kamu valign-text-middle">Toko Kamu</div>
-		<img class="settings" src="settings.png" alt="Settings"/>
+			<div class="toko-kamu valign-text-middle">Toko Kamu</div>
+			<a href="../setting">
+				<img src="../assets/svg/settings.png" width="20" height="20" alt="setting" />
+			</a>
 		</div>
 
-		<div class="layer-pertama">
-		<div class="layer-kedua">
-		<img class="foto-profil" src="portrait-of-happy-smiley-man.png" alt="Portrait of Happy Smiley Man"/>
-
-		<div class="layer-nama">
-		<div class="rico-putera-anugrah valign-text-middle">Rico Putera Anugrah</div>
-		<div class="frame-7">
-		<div class="ellipse-11"></div>
-		<div class="online valign-text-middle">Online</div>
-		</div>
-		</div>
-		</div>
-		</div>
+		<?php
+		//proses menampilkan data dari database:
+		//siapkan query SQL
+		$query = "SELECT * FROM toko WHERE owner = '$_SESSION[id]'";
+		$result = mysqli_query(connection(), $query);
+		?>
+		<?php while ($data = mysqli_fetch_array($result)) : ?>
+			<div class="frame-714">
+				<div class="frame-16">
+					<img src="../assets/toko/<?= $data['image_profile'] ?>" alt="Profil" style="height: 50px;min-width: 50px;object-fit: cover;position: relative;border-radius: 100%;" />
+					<div class="frame-15">
+						<div class="valign-text-middle" style="font-weight: bold;font-size: 1.25rem;"><?= $data['name'] ?></div>
+						<div class="frame-701">
+							<div class="ellipse-11"></div>
+							<div class="online valign-text-middle">Online</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
 
 		<div class="frame-container">
-			<img class="frame" src="produk.svg" alt="Produk"/> 
-			<img class="frame" src="bell.svg" alt="Bell"/>
-			<img class="frame" src="toko.svg" alt="Toko"/>
-			<img class="frame-717" src="kalkulator.svg" alt="Kalkulator"/> 
+			<a href="../beranda-mitra/"><img class="frame" src="../assets/svg/product.svg" alt="Frame 716" style="border-bottom: 2px solid #0002;" /></a>
+			<a href="../notifikasi/"><img class="frame" src="../assets/svg/notification.svg" alt="Frame 56" style="border-bottom: 2px solid #0002;" /></a>
+			<img class="frame" src="../assets/svg/catalog-active.svg" alt="Frame 57" style="border-bottom: 2px solid #3B82F6;" />
+			<a href="../penjualan/"><img class="frame-717" src="../assets/svg/stats.svg" alt="Frame 717" style="border-bottom: 2px solid #0002;" /></a>
 		</div>
-		
+
 		<div class="layer-bawah">
 
-		<div class="layer-list-produk">
-		<div class="list-produk-toko">List Produk Toko</div>
-				</div>
+			<div class="layer-list-produk">
+				<div class="list-produk-toko">List Produk Toko</div>
+			</div>
 
 
-		<div class="layer-produk-bawah">
+			<div class="layer-produk-bawah">
 
-		<div class="flex-col">
+				<div class="flex-col">
 
-		<div class="frame-72">
+					<div class="frame-72">
 
-		<div class="frame-71">
-			
-		<img class="vector" src="vector-1.svg" alt="Vector"/>
+						<div class="frame-71">
 
-		<div class="tambahkan-produk inter-semi-bold-black-12px">Tambahkan Produk</div>
-		</div>
+							<img class="vector" src="vector-1.svg" alt="Vector" />
 
-		<img class="vector-1" src="vector.svg" alt="Vector"/>
-		</div>
+							<div class="tambahkan-produk inter-semi-bold-black-12px">Tambahkan Produk</div>
+						</div>
 
-		<div class="frame-72">
+						<img class="vector-1" src="vector.svg" alt="Vector" />
+					</div>
 
-		<div class="frame-71">
+					<div class="frame-72">
 
-		<img class="vector" src="vector-2.svg" alt="Vector"/>
+						<div class="frame-71">
 
-		<div class="semua-produk inter-semi-bold-black-12px">Semua Produk</div>
-		</div>
-		<img class="vector-1" src="vector.svg" alt="Vector"/>
-		</div>
+							<img class="vector" src="vector-2.svg" alt="Vector" />
+
+							<div class="semua-produk inter-semi-bold-black-12px">Semua Produk</div>
+						</div>
+						<img class="vector-1" src="vector.svg" alt="Vector" />
+					</div>
 
 
 </body>
+
 </html>
--- seller/penjualan/index.php
+++ seller/penjualan/index.php
@@ -1,50 +1,92 @@
+<?php
+session_start();
+include '../../conn.php';
+if (!isset($_SESSION['id_toko'])) {
+	header("Location: ../formulir-mitra");
+} elseif (!isset($_SESSION['id'])) {
+	header("Location: ../login");
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title></title>
 </head>
 <link rel="stylesheet" type="text/css" href="style-penjualan.css">
+
 <body>
 	<div class="penjualan">
 		<div class="background-atas">
-		<div class="toko-kamu valign-text-middle">Toko Kamu</div>
-		<img class="settings" src="settings.png" alt="Settings"/>
+			<div class="toko-kamu valign-text-middle">Toko Kamu</div>
+			<a href="../setting">
+				<img src="../assets/svg/settings.png" width="20" height="20" alt="setting" />
+			</a>
 		</div>
 
-		<div class="layer-satu">
-		<div class="layer-nama-foto">
-		<img class="foto-profil" src="portrait-of-happy-smiley-man.png" alt="Portrait of Happy Smiley Man"/>
-
-		<div class="layer-nama">
-		<div class="rico-putera-anugrah valign-text-middle">Rico Putera Anugrah</div>
-		<div class="layer-online">
-		<div class="ellipse"></div>
-		<div class="online valign-text-middle">Online</div>
-		</div>
-		</div>
-		</div>
-		</div>
+		<?php
+		//proses menampilkan data dari database:
+		//siapkan query SQL
+		$query = "SELECT * FROM toko WHERE owner = '$_SESSION[id]'";
+		$result = mysqli_query(connection(), $query);
+		?>
+		<?php while ($data = mysqli_fetch_array($result)) : ?>
+			<div class="frame-714">
+				<div class="frame-16">
+					<img src="../assets/toko/<?= $data['image_profile'] ?>" alt="Profil" style="height: 50px;min-width: 50px;object-fit: cover;position: relative;border-radius: 100%;" />
+					<div class="frame-15">
+						<div class="valign-text-middle" style="font-weight: bold;font-size: 1.25rem;"><?= $data['name'] ?></div>
+						<div class="frame-701">
+							<div class="ellipse-11"></div>
+							<div class="online valign-text-middle">Online</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
 
 		<div class="frame-container">
-			<img class="frame1" src="produk.svg" alt="Produk"/> 
-			<img class="frame2" src="bell.svg" alt="Bell"/>
-			<img class="frame3" src="toko.svg" alt="Toko"/>
-			<img class="frame4" src="kalkulator.svg" alt="Kalkulator"/> 
+			<a href="../beranda-mitra/"><img class="frame" src="../assets/svg/product.svg" alt="Frame 716" style="border-bottom: 2px solid #0002;" /></a>
+			<a href="../notifikasi/"><img class="frame" src="../assets/svg/notification.svg" alt="Frame 56" style="border-bottom: 2px solid #0002;" /></a>
+			<a href="../etalase/"><img class="frame" src="../assets/svg/catalog.svg" alt="Frame 57" style="border-bottom: 2px solid #0002;" /></a>
+			<img class="frame-717" src="../assets/svg/stats-active.svg" alt="Frame 717" style="border-bottom: 2px solid #3B82F6;" />
 		</div>
 
+		<?php
+		$query_produk = "SELECT * FROM product WHERE toko_id = '$_SESSION[id_toko]'";
+		$result_produk = mysqli_query(connection(), $query_produk);
+		$total_produk = 0;
+		while ($data_produk = mysqli_fetch_array($result_produk)) {
+			$total_produk++;
+		}
 
-		<h1 class="title">Laporan toko anda</h1>
-
-		<div class="saat-ini">Saat ini</div>
-
-		<div class="keseluruhan">Keseluruhan</div>
-
-		<img class="garis" src="garis.svg" alt="Garis"/>
-
-		<div class="box" align="center"><p>Total Produk<br/><b>00</b></p></div>
-		<br/>
-		<div class="total-pendapatan">
-		<span class="total">Total Pendapatan: </span>
-		<span class="span1">Rp 0</span></div>
+		$query = "SELECT * FROM toko WHERE owner = '$_SESSION[id]'";
+		$result = mysqli_query(connection(), $query);
+		?>
+		<?php while ($data_toko = mysqli_fetch_array($result)) : ?>
+			<div className="p-6" style="padding: 1.5rem;">
+				<h1 className="text-2xl font-normal text-slate-700" style="font-size: 1.5rem;color: rgb(51 65 85);">Laporan Toko Anda</h1>
+				<div>
+					<div className="py-3 [&>*:hover]:cursor-pointer [&>p:hover]:border-mygreen_dark w-min text-mygreen_dark" style="padding: .75rem 0;cursor: pointe;width: min-content;color: #3B82F6;">
+						<p className="p-1 text-xs font-semilight border-b-2 opacity-60 hover:opacity-100" style="padding: .25rem;font-size: .75rem;font-weight: 300;border-bottom: 2px solid #3b82f6;">
+							Keseluruhan
+						</p>
+					</div>
+					<div className="py-3" style="padding: .75rem 0;">
+						<div className="py-3 px-6 text-white rounded-xl bg-mygreen_dark w-36" style="padding: .75rem 1.5rem;color: white;border-radius: .75rem;background-color: #3B82F6;width: 9rem;">
+							<h1 style="font-size: 1.25rem;">Total Produk</h1>
+							<span className="text-2xl font-bold" style="font-size: 1.5rem;font-weight: bold;"><?= $total_produk ?></span>
+						</div>
+						<h1 className="my-3 text-md font-normal text-slate-700" style="margin: .75rem 0;color: rgb(51 65 85);font-size: 1.25rem;">
+							Pendapatan Total :
+							<span className="font-bold text-xl text-slate-600 tracking-wide" style="font-weight: bold;font-size: 1.25rem;color: rgb(71 85 105);">
+								Rp. <?= $data_toko['pendapatan_total'] ?>
+							</span>
+						</h1>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
 </body>
+
 </html>
