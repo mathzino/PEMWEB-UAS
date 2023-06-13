247a5301d700e63adcdbee574f864e5875171374
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-14 01:00:38 +0700

    [ADD] give rating

---
+++ buyer/handle_rating.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include '../conn.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id_toko = $_SESSION['id_toko'];
+    $id_transaction = $_POST['id_transaction'];
+    $id_produk = $_POST['id_produk'];
+    $rating = $_POST['rating'] > 5 ? 5 : $_POST['rating'];
+    $rating = $rating < 1 ? 1 : $rating;
+
+    $query = "INSERT INTO star VALUES('','$id_produk','$rating')";
+
+    $result = mysqli_query(connection(), $query);
+    if ($result) {
+        header('Location: ./notify.php?status=rating');
+    } else {
+        $status = 'err';
+        header('Location: ratingPage.php?status=' . $status);
+    }
+}
--- buyer/notifyDetail.php
+++ buyer/notifyDetail.php
@@ -61,7 +61,7 @@ if (isset($_GET["id_transaction"])) {
             <div style="display: flex; justify-content: space-between">
               <div style="width: 6rem; height: 1.75rem; background-color: #dbeafe; border-radius: 1.125rem">
                 <a href="/seller"
-                  style="background: #3b82f6; padding: 16px 6px; text-align: center; border-radius: 24px">
+                  style="background: #3b82f6; padding: 16px 6px; text-align: center; border-radius: 24px;text-decoration:none">
                   <p
                     style="font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 12px; line-height: 126.02%; color: white; text-decoration: none">
                     Klik disini</p>
@@ -144,22 +144,22 @@ if (isset($_GET["id_transaction"])) {
                 <div
                   style="width: 88px; height: 88px; border-radius: 100%; background-color: #cbcbcb; position: relative; overflow: hidden">
                   <img style="object-fit: cover; width: 100%; height: 100%"
-                    src=<?php $transactionInformation['image_profile']?>
-                    alt="" />
+                    src="../seller/assets/toko/<?= $transactionInformation['image_profile']?>"
+                    alt="toko" />
                 </div>
                 <div style="display: flex; flex-direction: column; justify-content: center">
                   <div style="display: flex; gap: 4px; align-items: center">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M22.5 8.01001L21 3.51001C20.9479 3.35478 20.8463 3.22089 20.7109 3.1288C20.5754 3.03671 20.4136 2.99148 20.25 3.00001H3.75005C3.5865 2.99148 3.42465 3.03671 3.28922 3.1288C3.15379 3.22089 3.05223 3.35478 3.00005 3.51001L1.50005 8.01001C1.48924 8.08965 1.48924 8.17038 1.50005 8.25001V12.75C1.50005 12.9489 1.57906 13.1397 1.71972 13.2803C1.86037 13.421 2.05114 13.5 2.25005 13.5H3.00005V21H4.50005V13.5H9.00005V21H21V13.5H21.75C21.949 13.5 22.1397 13.421 22.2804 13.2803C22.421 13.1397 22.5 12.9489 22.5 12.75V8.25001C22.5109 8.17038 22.5109 8.08965 22.5 8.01001ZM19.5 19.5H10.5V13.5H19.5V19.5ZM21 12H18V9.00001H16.5V12H12.75V9.00001H11.25V12H7.50005V9.00001H6.00005V12H3.00005V8.37001L4.29005 4.50001H19.71L21 8.37001V12Z"
-                        fill="#618D80" />
+                        fill="#3b82f6" />
                     </svg>
                     <div
-                      style="font-family: 'Inter'; font-style: normal; font-weight: 400; font-size: 16px; line-height: 19px; color: #618d80">
+                      style="font-family: 'Inter'; font-style: normal; font-weight: 400; font-size: 16px; line-height: 19px; color: #3b82f6">
                       <?php echo $transactionInformation['store_name'] ?></div>
                   </div>
                   <div
-                    style="font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 16px; line-height: 19px; color: #618d80; margin-top: 12px; margin-bottom: 7px">
+                    style="font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 16px; line-height: 19px; color: #3b82f6; margin-top: 12px; margin-bottom: 7px">
                     Rp <?php echo $total ?></div>
                 </div>
               </div>
@@ -178,8 +178,20 @@ if (isset($_GET["id_transaction"])) {
                     margin-top: 30px;
                     padding: 8px 0;
                   ">
+                <?php if($transactionInformation['status'] == 0):?>
                 Menunggu Konfirmasi
+                <?php elseif($transactionInformation['status'] == 1):?>
+                Pesanan telah disetujui, silahkan ditunggu..
+                <?php elseif($transactionInformation['status'] == 2):?>
+                  <form action="ratingPage.php" method="GET">
+                  <input type="hidden" name="id_transaction" value="<?php echo $transactionInformation['id_transaction']?>">
+                    <button type="submit" style="border:none;background-color:transparent;color:white;cursor:pointer;">Beri Rating</button>
+                  </form>
+                <?php elseif($transactionInformation['status'] == 3):?>
+                Ditolak
+                <?php endif;?>
               </div>
+
 
               <div
                 style="font-family: 'Inter'; font-style: normal; font-weight: 400; font-size: 16px; line-height: 19px; color: #3b82f6; text-align: center; margin-top: 20px">
